d1b74634581b7fd252450ab65162cf7d97b30c36
slthurman01 <slthurman01@gmail.com>
2023-02-07 11:42:56 -0600

    Fixes bugs, adds fs dependency, adds README.

--- Develop/server.js
+++ Develop/server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+let notes = [];
+
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
 
 app.use(express.static('public'));
 
@@ -35,7 +37,7 @@ app.post('/api/notes', (req, res) => {
         JSON.stringify({ notes }, null, 2)
     );
     res.json(notes);
-}
+});
 
 // DELETE Route for deleting notes
 app.delete('/api/notes/:id', (req, res) => {
@@ -48,6 +50,6 @@ app.delete('/api/notes/:id', (req, res) => {
     res.json(notes);
 });
 
-    app.listen(PORT, () =>
-    console.log(`App listening at http://localhost:${PORT} 🚀`)
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
 );
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# Note Taker Starter Code
+
+  # Note Taker
+
+  ## Description
+  An application that can write and save notes.
+  ![GitHub license]()
+  
+  [Link to License]()
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Links](#links)
+  - [Questions](#questions)
+  
+  ## Installation
+  Run using ```npm run start```
+  
+  ## Usage
+  Write a note title and a note on the main page after redirecting from the starting splash page.  Click the save icon to save the note.
+  
+  ## Links
+  None
+  
+  ## Questions
+  GitHub Username:
+
+  [skip-thurm](https://github.com/skip-thurm)
+
+  You can reach me with any questions at:
+  
+  <slthurman01@gmail.com>
